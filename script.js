80044421855928ca808f29f6c41746363b9616b8
Soumyajit Paria <soumyajitparia2005@gmail.com>
2024-08-03 13:48:19 +0530

    responsive design

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-const showmodal = () =>{
-    document.querySelector('.overlay').classList.add('showoverlay');
-    document.querySelector('.loginform').classList.add('showloginform');
-}
+const showmodal = () => {
+  document.querySelector(".overlay").classList.add("showoverlay");
+  document.querySelector(".loginform").classList.add("showloginform");
+};
 
-const closemodal = () =>{
-    document.querySelector('.overlay').classList.remove('showoverlay');
-    document.querySelector('.loginform').classList.remove('showloginform');
-}
+const closemodal = () => {
+  document.querySelector(".overlay").classList.remove("showoverlay");
+  document.querySelector(".loginform").classList.remove("showloginform");
+};
 
-document.getElementsByClassName('register-button')[0].addEventListener('click', showmodal);
-let closeButton = document.querySelector('.modalCloseButton');
-closeButton.addEventListener('click', closemodal);
+document
+  .getElementsByClassName("register-button")[0]
+  .addEventListener("click", showmodal);
+let closeButton = document.querySelector(".modalCloseButton");
+closeButton.addEventListener("click", closemodal);
